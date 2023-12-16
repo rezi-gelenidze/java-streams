aa8f3935a0669e60c9985840f76918f05575af2f
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2023-12-16 17:42:41 +0400

    add Function.compose and Function.andThen methods

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 import collection.List;
-import function.Consumer;
-import function.Predicate;
+import function.*;
 import stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Function<String, Integer>
-        Stream.of(new String[]{"1 ", "2", "3", " 4"})
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .map((a) -> 2 * a)
+        Function<String, Integer> a = Integer::parseInt;
+        Function<Integer, Double> b = i -> (double) (i / 2);
+
+        Stream.of(new String[]{"1", "2", "3", "4"})
+                .map(b.compose(a))
                 .forEach(System.out::println);
     }
 }
--- src/function/Function.java
+++ src/function/Function.java
@@ -3,11 +3,11 @@ package function;
 public interface Function<T,R> {
     R apply (T arg);
 
-//    default Function<T, R> compose(Function<? super T, ? extends R> before){
-//        return (R t) -> this.apply(before.apply(t));
-//    }
-//
-//    default Function<T, R> andThen(Function<? super T, ? extends R> after) {
-//        return (T t) -> after.apply(this.apply(t));
-//    }
+    default <V> Function<V, R> compose(Function<? super V, ? extends T> before){
+        return (V v) -> this.apply(before.apply(v));
+    }
+
+    default <V> Function<T, V> andThen(Function<? super R, ? extends V> after) {
+        return (T t) -> after.apply(this.apply(t));
+    }
 }
